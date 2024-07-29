67d3494c7a4e7977e720f31ad828270ada2e0fcd
Nishi Choudhary <94692104+nishichoudhary@users.noreply.github.com>
2024-07-29 18:47:08 +0530

    Add files via upload

---
+++ Week 1/Design Patters and Principles/Exercise 5: Decorator Pattern/NotifierDecorator.java
@@ -0,0 +1,16 @@
+package DecoratorPatternExample;
+
+public abstract class NotifierDecorator implements Notifier
+{
+    protected Notifier wrappedNotifier;
+
+    public NotifierDecorator(Notifier notifier)
+    {
+        this.wrappedNotifier = notifier;
+    }
+
+    public void send(String message)
+    {
+        wrappedNotifier.send(message);
+    }
+}
