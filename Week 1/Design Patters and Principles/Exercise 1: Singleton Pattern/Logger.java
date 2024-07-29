7fe01fd35e320d1282ddde884efbedcf4c6ee867
Nishi Choudhary <94692104+nishichoudhary@users.noreply.github.com>
2024-07-29 18:02:11 +0530

    Create Logger.java

---
+++ Week 1/Design Patters and Principles/Exercise 1: Singleton Pattern/Logger.java
@@ -0,0 +1,21 @@
+package SingletonPattern;
+
+public class Logger
+{
+    private static Logger instance;
+    private Logger() {}
+
+    public static Logger getInstance()
+    {
+        if (instance == null)
+        {
+            synchronized (Logger.class) {
+                if (instance == null)
+                {
+                    instance = new Logger();
+                }
+            }
+        }
+        return instance;
+    }
+}
